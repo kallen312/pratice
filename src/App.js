47ff2520094b472ad4ce58f8bdbd9ec9f3045be1
kallen312 <73615486+kallen312@users.noreply.github.com>
2022-01-12 18:30:53 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from "react";
+//import "./App.css";
+
+/*
+1. Create a new functional component called IceCream that returns a div tag
+2. Inside the div tag, create a paragraph tag that displays a flavor using props
+  Name your prop 'flavor'
+3. In App, add three IceCream components that receive your flavor 
+  props strawberry, chocolate, and vanilla
+*/
+
+
+
+function IceCream(props){
+  return (
+<div> 
+<p> {props.flavor} </p>
+
+</div>
+  );
+}
+
+function App() {
+  return (
+  <div className="app">
+    <h1>Menu Choices</h1>
+  
+  <IceCream  flavor = "Chocalte" />
+  
+  </div>
+  );
+};
+
+export default App;
+
+ 
